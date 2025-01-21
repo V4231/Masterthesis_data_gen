3cfbb37f569ba0e3936a593857a971ab344d49bc
Vincent_Office <Vincent.sprave@st.ovgu.de>
2025-01-21 08:19:07 +0100

    added plot

--- src/fir_data_plot.py
+++ src/fir_data_plot.py
@@ -4,7 +4,8 @@ import glob
 import numpy as np
 import pandas as pd
 
-csv_files = ["input_fir.csv", "output.csv"]
+csv_files = ["input_fir.csv", "output0.csv"]
+plot_names = ["Frequency unfiltered", "Frequency filtered"]
 
 data_frames = []
 for file in csv_files:
@@ -14,9 +15,15 @@ for file in csv_files:
     df.drop(columns=df.columns[4],axis=1, inplace=True)
     df.drop(columns=df.columns[4],axis=1, inplace=True)
     data_frames.append(df)
+    #print(df)
     
 size = 1000
 x = np.linspace(0, 1,size)
+x_axis = [
+    x,
+    np.delete(x,[1,2,3])
+]
+print(x_axis)
 colors = ["green", "red"]
 fig = make_subplots(rows=4, cols=1)
 for idx,df in enumerate(data_frames):
@@ -24,10 +31,10 @@ for idx,df in enumerate(data_frames):
     for column_name, columns in df.items():
         fig.add_trace(
             go.Scatter(
-                    x=x,
+                    x=x_axis[idx],
                     y=columns,
                     mode="lines",
-                    name='Frequency ' + str(i),
+                    name=plot_names[idx] + str(idx),
                     marker=dict(color=colors[idx], size=10)
                 ),
             row=i,
